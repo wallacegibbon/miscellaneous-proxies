2e63c2a6bb5be2c18100b15f37b7ee69c020ff1b
wallacegibbon <wallacegibbon@aliyun.com>
2025-05-27 22:36:55 +0800

    Improvement: Since more clients will be supported, postfixes got introduced.

---
+++ start-v2ray.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+SCRIPTDIR=$(dirname $0)
+
+if test $# -lt 1; then
+	echo "The config file ID is not specified" >&2
+	exit 1
+fi
+
+server=$(cat $SCRIPTDIR/db-v2ray/$1*/server)
+tag=$(cat $SCRIPTDIR/db-v2ray/$1*/tag)
+
+echo
+echo "server config:" $server
+echo "server tag:" $tag
+
+sed "s|<SERVER>|$server|" $SCRIPTDIR/config-v2ray.json.tmpl \
+	> $SCRIPTDIR/config-v2ray.json
+
+v2ray -config $SCRIPTDIR/config-v2ray.json
+
--- update-v2ray.sh
+++ update-v2ray.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
-## ~/V2RAY.sub should contain an URL like:
+PROXY_URL_FILE="$HOME/PROXY.SUB"
+
+## PROXY_URL_FILE should contain an URL like:
 ##	https://your.site.com/subscribe?token=yoursecrettoken
 
-if ! grep -q '^http' ~/V2RAY.sub 2> /dev/null; then
-	echo "Valid ~/V2RAY.sub is not found" >&2
+if ! grep -q '^https\?://' $PROXY_URL_FILE 2> /dev/null; then
+	echo "No valid $PROXY_URL_FILE found" >&2
 	exit 1
 fi
 
 SCRIPTDIR=$(dirname $0)
 
-rm -rf $SCRIPTDIR/db
-mkdir $SCRIPTDIR/db
+rm -rf $SCRIPTDIR/db-v2ray
+mkdir $SCRIPTDIR/db-v2ray
 
 f() {
 	awk -F ':' "{print \$$1}"
@@ -27,10 +29,10 @@ extract() {
 	password=$(echo $method_pass | f 2)
 	address=$(echo $a | f 2)
 	port=$(echo $a | f 3)
-	tagraw=$(echo $a | f 4)
+	tagraw=$(echo $a | f 4 | tr -d '\r')
 	tag=$(printf "%b\n" "${tagraw//%/\\x}")
 
-	p="$SCRIPTDIR/db/$(printf "%03d" $id).$tag"
+	p="$SCRIPTDIR/db-v2ray/$(printf "%03d" $id).$tag"
 	mkdir "$p"
 
 	printf '{"address":"%s", "method":"%s", "password":"%s", "port":%d}' \
@@ -43,7 +45,7 @@ extract() {
 
 id=0
 
-for l in $(curl -s $(cat ~/V2RAY.sub) | base64 -d | grep '^ss:'); do
+for l in $(curl -s $(cat $PROXY_URL_FILE) | base64 -d | grep '^ss:'); do
  	printf "%s\n" "$l" | extract $id
 	((id++))
 done
