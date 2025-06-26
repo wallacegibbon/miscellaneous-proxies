e0905496ccedf569576ce43fb11b56fd94e47116
wallacegibbon <wallacegibbon@aliyun.com>
2025-06-26 14:31:29 +0800

    Feature: DIRECTDOMAIN and DIRECTIP (as env) are supported by `start-v2ray.sh`.

---
+++ README.md
@@ -0,0 +1,12 @@
+## Introduction
+
+Shell scripts for managing shadowsocks links and starting the v2ray client.
+
+
+## Tips
+
+Add custome direct domain or IP:
+
+```sh
+DIRECTIP=31.13.95.18,59.24.3.174 DIRECTDOMAIN=a.com,b.com ./start-v2ray.sh 000
+```
--- start-v2ray.sh
+++ start-v2ray.sh
@@ -19,7 +19,19 @@ tag=$(cat $SCRIPTDIR/db-v2ray/$1*/tag)
 #echo "server config:" $server
 echo "server tag:" $tag
 
-sed "s|<SERVER>|$server|" $SCRIPTDIR/config-v2ray.json.tmpl \
+echo Custom direct domains: $DIRECTDOMAIN
+echo Custom direct IPs: $DIRECTIP
+
+if test -n "$DIRECTDOMAIN"; then
+	DIRECTDOMAIN=",\"$(echo $DIRECTDOMAIN | sed 's/,/","/g')\""
+fi
+
+if test -n "$DIRECTIP"; then
+	DIRECTIP=",\"$(echo $DIRECTIP | sed 's/,/","/g')\""
+fi
+
+sed "s|<SERVER>|$server|;s|<DIRECTIP>|$DIRECTIP|;s|<DIRECTDOMAIN>|$DIRECTDOMAIN|" \
+	$SCRIPTDIR/config-v2ray.json.tmpl \
 	> $SCRIPTDIR/config-v2ray.json
 
 v2ray -config $SCRIPTDIR/config-v2ray.json
